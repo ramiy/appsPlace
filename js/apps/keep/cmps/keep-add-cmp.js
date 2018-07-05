afdf981e4a7baa8f6fe690237dc94592514af43d
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-05 15:54:45 +0300

    Initial keep app

---
+++ js/apps/keep/cmps/keep-add-cmp.js
@@ -0,0 +1,14 @@
+export default {
+	template: `
+		<section class="keep-app">
+
+			<input type="type" @input="saveKeep">
+
+		</section>
+    `,
+	methods: {
+		saveKeep() {
+
+		}
+	},
+}
--- js/apps/keep/pages/keep-app-cmp.js
+++ js/apps/keep/pages/keep-app-cmp.js
@@ -1,4 +1,79 @@
+import keepAdd from '../cmps/keep-add-cmp.js';
+
 export default {
 	template: `
+		<section class="keep-app">
+
+			<keep-add></keep-add>
+
+			<!-- <button @click="cmps.push({cmpType: 'say-hello', data: {greet : 'Hi'}})">+</button> -->
+
+			<component v-for="(cmp, idx) in keepCmps" :is="cmp.keepType" :key="idx" :data="cmp.data">
+			</component>
+
+		</section>
 	`,
+	data() {
+		return {
+			currView: '',
+			keepCmps: [
+				{ keepType: 'keep-text', data: { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' } },
+				{ keepType: 'keep-text', data: { text: 'Another lorem ipsum dolor' } },
+				{ keepType: 'keep-image', data: { src: 'http://placehold.it/300x400?text=Image' } },
+				{ keepType: 'keep-text', data: { text: 'Blah blah blah ...' } },
+				{ keepType: 'keep-note', data: { text: 'note info...' } },
+			],
+		}
+	},
+	components: {
+		keepAdd,
+		'keep-text': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-text">
+					{{data.text}}
+				</section>
+			`
+		},
+		'keep-image': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-image">
+					<img :src="data.src" alt="" />
+				</section>
+			`
+		},
+		'keep-note': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-note">
+					{{data.text}}
+				</section>
+			`
+		},
+		'keep-video': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-video">
+					video
+				</section>
+			`
+		},
+		'keep-audio': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-audio">
+				audio
+				</section>
+			`
+		},
+		'keep-list': {
+			props: ['data'],
+			template: `
+				<section class="keep-item keep-list">
+					list
+				</section>
+			`
+		},
+	}
 }
