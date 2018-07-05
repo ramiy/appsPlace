99532d6f74b7f49d0e228ea90cd626a0f3dd01f5
Itai Yosephi <itai.yosephi@gmail.com>
2018-07-05 15:17:16 +0300

    added emails data

--- js/apps/email/pages/email-app-cmp.js
+++ js/apps/email/pages/email-app-cmp.js
@@ -1,16 +1,28 @@
+import emailService from '../services/email-service.js'
+
 export default {
 	template: `
 		<section class="email-app">
-
+			<h1>Emails App</h1>
         
         </section>
     
     
     
     `,
-    data() {
-        return {
 
-        }
-    }
+	data() {
+		return {
+			emails: [],
+			filter: null,
+		}
+	},
+	created() {
+		emailService.query()
+			.then(emails => {
+				this.emails = emails;
+				console.log(this.emails);
+				
+			})
+	},
 }
