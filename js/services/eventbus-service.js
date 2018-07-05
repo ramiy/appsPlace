2ed05145cf5bc6c84306bf9526724c93a15b4033
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-05 14:19:46 +0300

    Add event buss

---
+++ js/services/eventbus-service.js
@@ -0,0 +1,7 @@
+export const EVENT_KEEP_SAVED = 'EVENT_KEEP_SAVED';
+export const EVENT_KEEP_DELETED = 'EVENT_KEEP_DELETED';
+
+export const EVENT_EMAIL_SAVED = 'EVENT_EMAIL_SAVED';
+export const EVENT_EMAIL_DELETED = 'EVENT_EMAIL_DELETED';
+
+export const eventBus = new Vue();
