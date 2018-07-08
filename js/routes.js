40eabfca9a2036aeef982cdceb3a68a4f678095a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-08 11:59:33 +0300

    remove unused note-details component

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,10 @@
 import homePage from './pages/home-cmp.js';
 import emailApp from './apps/email/pages/email-app-cmp.js';
 import notesApp from './apps/keep/pages/notes-app-cmp.js';
-import notesDetails from './apps/keep/pages/notes-details-cmp.js';
 
 export default [
 	{ path: '/', component: homePage },
 	{ path: '/email/:emailId?', component: emailApp },
 	{ path: '/notes', component: notesApp },
-	{ path: '/notes/:noteId', component: notesDetails },
 	{ path: '*', redirect: '/' },
 ]
