7d9b0542988bbbc2a819b4e60b8dbc0c6534161d
Itai Yosephi <itai.yosephi@gmail.com>
2018-07-08 16:12:04 +0300

    nothing

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,12 @@
 import homePage from './pages/home-cmp.js';
 import emailApp from './apps/email/pages/email-app-cmp.js';
+import emailDetails from './apps/email/cmps/email-details-cmp.js';
 import notesApp from './apps/keep/pages/notes-app-cmp.js';
 
 export default [
 	{ path: '/', component: homePage },
 	{ path: '/email/:emailId?', component: emailApp },
+	{ path: '/email/:emailId/show', component: emailDetails },
 	{ path: '/notes', component: notesApp },
 	{ path: '*', redirect: '/' },
 ]
