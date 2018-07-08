bddba333153335d67a11d43e94a98cb694788b07
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-08 18:47:44 +0300

    update email filtering

--- js/apps/email/cmps/email-filter-cmp.js
+++ js/apps/email/cmps/email-filter-cmp.js
@@ -3,92 +3,46 @@ export default {
 
 	template: `
 		<section class="email-filter">
-                <i class="fas fa-search  fa-lg" @click="setFilter"></i>
-				<input v-model="filter.txt" type="search" placeholder="Search mail" @keyup.13="setFilter">
-				<h3 @click="show=!show">
-					<p>{{emailStatus}}</p> <i class="fas fa-sort-down"></i>
-					<ul class="email-status" v-show="show" >
-						<li>
-							<label>
-							All
-							<input value="all" type="radio" v-model="status">
-
-							</label>
-							
-						</li>
-						<li>
-							<label>
-							Read
-							<input value="read" type="radio" v-model="status">
-
-							</label>
-							
-						</li>
-						<li>
-							<label >
-							Unread
-							<input  value="unread" type="radio" v-model="status">
-
-							</label>
-							
-						</li>
-
-					</ul>
-				</h3>
-        
-
+			<i class="fas fa-search  fa-lg" @click="setFilter"></i>
+			<input v-model="filter.txt" type="search" placeholder="Search mail" @keyup.13="setFilter">
+			<div class="filter" @click="show=!show">
+				<p>{{emailStatus}}</p> <i class="fas fa-sort-down"></i>
+				<ul class="email-status" v-show="show" >
+					<li><label>All <input value="all" type="radio" v-model="status"></label></li>
+					<li><label>Read <input value="read" type="radio" v-model="status"> </label></li>
+					<li><label>Unread <input value="unread" type="radio" v-model="status"> </label></li>
+				</ul>
+			</div>
 		</section>
-    
     `,
 	data() {
 		return {
 			filter: {
 				txt: '',
 				emailStatus: 'all'
-
 			},
 			status: 'all',
 			show: false
-
 		}
-		
-
-	},
-	created() {
-
-	},
-	components: {
 	},
 	methods: {
 		toggleShow() {
-			console.log('toggle');
-			
-			this.show = !this.show
-		
+			this.show = !this.show;
 		},
 		setFilter() {
-			console.log('sssss');
-
-			this.$emit('set-filter', this.filter)
+			this.$emit('set-filter', this.filter);
 		}
-
 	},
 	computed: {
 		emailStatus() {
-
 			return this.filter.emailStatus.charAt(0).toUpperCase() + this.filter.emailStatus.slice(1);
-
 		}
-
-
 	},
 	watch: {
 		status() {
 			this.toggleShow()
-			
 			this.filter.emailStatus = this.status;
-			this.$emit('set-filter', this.filter)
-
+			this.$emit('set-filter', this.filter);
 		}
 	}
 }
