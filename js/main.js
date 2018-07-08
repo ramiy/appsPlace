61a9ddb5f4bc32662d06373673c2d307b33713d6
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-07-08 18:53:08 +0300

    global search from the header for all apps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import appRoutes from './routes.js'
 import { eventBus, EVENT_ACTIVE_APP_SET } from './services/eventbus-service.js'
-
+import appRoutes from './routes.js'
+import notesFilter from './apps/keep/cmps/notes-filter-cmp.js';
+import emailFilter from './apps/email/cmps/email-filter-cmp.js';
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,10 @@ new Vue({
 	router: appRouter,
 	data: {
 		activeApp: ''
+	},
+	components: {
+		notesFilter,
+		emailFilter
 	},
 	created() {
 		eventBus.$on(EVENT_ACTIVE_APP_SET, appName => this.activeApp = appName);
